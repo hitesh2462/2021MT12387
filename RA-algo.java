a337ad97c80d49a8e8f31af38401c783ce342e8a
hitesh2462 <55327517+hitesh2462@users.noreply.github.com>
2021-11-03 12:11:04 +0530

    Delete RA-algo.java

--- RA-algo.java
+++
@@ -1,8 +0,0 @@
-public class RA{
-
-  public static void main(String[] args) {
-
-    Site siteObj = new Site(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-    siteObj.Site_main();
-  }
-}
