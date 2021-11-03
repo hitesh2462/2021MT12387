65745432d4e1a58f78ed5a31275db5453c086b2f
hitesh2462 <55327517+hitesh2462@users.noreply.github.com>
2021-11-03 12:06:04 +0530

    Add files via upload

---
+++ RA-algo.java
@@ -0,0 +1,8 @@
+public class RA{
+
+  public static void main(String[] args) {
+
+    Site siteObj = new Site(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    siteObj.Site_main();
+  }
+}
